0a5f0e64efabef7a2cd5bf08b13621816fcf47a3
vunderkind <justin.irabor@gmail.com>
2019-10-15 07:02:35 +0100

    fixed display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,33 @@
 // Test away
+
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
+
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+let control;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Dashboard />);
+    control = rtl.render(<Controls />)
+})
+
+
+describe('Control Toggles On Click of Button', () => {
+    
+    //on startup, test that the buttons rendered are the default buttons
+    // it('Indicator is locked when gate is locked', () => {
+    //     expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    //     rtl.fireEvent.click(control.queryByText(/close gate/i));
+    //     // rtl.fireEvent.click(control.queryAllByText(/lock gate/i));
+    //     // expect(wrapper.queryByText(/unlocked/i)).not.toBeInTheDocument();
+
+    // })
+
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Display />)
+})
+
+describe("Testing what is displayed", () => {
+test("displays closed if the closed prop is true", () => {
+    const wrapper = rtl.render(<Display closed={true}/>);
+    expect(wrapper.queryByText(/closed/i)).toBeInTheDocument(); 
+    expect(wrapper.queryByText(/open/i)).toBeVisible();    
+});
+
+});
