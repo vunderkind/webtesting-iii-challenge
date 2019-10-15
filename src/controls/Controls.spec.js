a81cef29d3edf507b28f26b568ffa2e1c25459ad
vunderkind <justin.irabor@gmail.com>
2019-10-15 06:27:03 +0100

    included Controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = rtl.render(<Controls />)
+})
+
+
+describe('Control Toggles On Click of Button', () => {
+    
+    //on startup, test that the buttons rendered are the default buttons
+    it('renders properly', () => {
+        expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+        rtl.fireEvent.click(wrapper.queryByText(/close gate/i));
+        expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open gate/i)).not.toBeInTheDocument();
+    })
+
+})
+
